31be5c2fb8560675ee17719aae6a20834272eaa0
libnist <ghahremani.mim@gmail.com>
2025-03-06 20:46:51 +0330

    fix probmel in delivered column

--- frontend/src/components/screens/OrderListScreen.jsx
+++ frontend/src/components/screens/OrderListScreen.jsx
@@ -57,7 +57,7 @@ export default function OrderListScreen() {
                                 <td>{order.createdAt.substring(0, 10)}</td>
                                 <td>${order.totalPrice}</td>
                                 <td>{order.isPaid ? order.paidAt.substring(0, 10) : <i className="fas fa-times" style={{ color: "red" }}></i>}</td>
-                                <td>{order.isDelivred ? order.deliveredAt.substring(0, 10) : <i className="fas fa-times" style={{ color: "red" }}></i>}</td>
+                                <td>{order.isDelivered ? order.deliveredAt.substring(0, 10) : <i className="fas fa-times" style={{ color: "red" }}></i>}</td>
                                 <td>
                                     <Link to={`/order/${order._id}`}>
                                         <Button variant="light" className="btn-sm">Details</Button>
